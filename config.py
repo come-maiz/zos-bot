f96122c057a2a6d1f3f69598cbc9c8d95f0d05ee
Leo Arias <yo@elopio.net>
2018-09-24 13:30:18 +0000

    Update the config to run inside the snap

--- config.py
+++ config.py
@@ -3,11 +3,6 @@ import os
 
 BACKEND = 'TelegramPatched'
 
-BOT_DATA_DIR = r'/home/ubuntu/workspace/elopio/zos-bot/data'
-BOT_EXTRA_PLUGIN_DIR = r'/home/ubuntu/workspace/elopio/zos-bot/plugins'
-BOT_EXTRA_BACKEND_DIR = r'/home/ubuntu/workspace/elopio/zos-bot/backends'
-
-
 BOT_LOG_FILE = r'/home/ubuntu/workspace/elopio/zos-bot/errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
@@ -20,7 +15,7 @@ BOT_IDENTITY = {
 CHATROOM_PRESENCE = ()
 BOT_PREFIX = '/'
 CORE_PLUGINS = ()
-#BOT_DATA_DIR = os.environ.get('SNAP_USER_DATA')
-#BOT_EXTRA_PLUGIN_DIR = os.path.join(os.environ.get('SNAP'), 'plugins'
-#BOT_EXTRA_BACKEND_DIR = os.path.join(os.environ.get('SNAP'), 'backends'
-#BOT_LOG_FILE = BOT_DATA_DIR + '/err.log'
+BOT_DATA_DIR = os.environ.get('SNAP_USER_DATA')
+BOT_EXTRA_PLUGIN_DIR = os.path.join(os.environ.get('SNAP'), 'plugins')
+BOT_EXTRA_BACKEND_DIR = os.path.join(os.environ.get('SNAP'), 'backends')
+BOT_LOG_FILE = os.path.join(BOT_DATA_DIR, 'err.log'
