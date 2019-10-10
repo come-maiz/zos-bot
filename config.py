3e7ee00974910e868b9bc7efe36f7abc72680d6a
Leo Arias <elopio@openzeppelin.com>
2019-10-09 22:27:30 -0600

    Remove duplicated entry

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 BACKEND = 'TelegramPatched'
 
-BOT_LOG_FILE = r'/home/ubuntu/workspace/elopio/zos-bot/errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
 TELEGRAM_ID_ELOPIO = '43624396'
@@ -18,4 +17,6 @@ CORE_PLUGINS = ()
 BOT_DATA_DIR = os.environ.get('SNAP_USER_DATA')
 BOT_EXTRA_PLUGIN_DIR = os.path.join(os.environ.get('SNAP'), 'plugins')
 BOT_EXTRA_BACKEND_DIR = os.path.join(os.environ.get('SNAP'), 'backends')
+
+BOT_LOG_LEVEL = logging.DEBUG
 BOT_LOG_FILE = os.path.join(BOT_DATA_DIR, 'err.log')
